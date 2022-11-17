ce80a7f04c81b187d8a61374398861c234a707ef
ronaldgreen110 <geniusdev1108@gmail.com>
2022-11-17 14:58:07 -0600

    Upload Project

---
+++ contracts/TestUSDC.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestUSDC is ERC20 {
+    constructor(string memory _name, string memory _symbol)
+        ERC20(_name, _symbol)
+    {}
+
+    function mint(uint256 _amount) external {
+        _mint(msg.sender, _amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+}
